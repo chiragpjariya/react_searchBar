89f0c1779c3eab1977562faaa572481deff28aa3
chirag jariya <chiragjariya0109@gmail.com>
2025-03-03 10:13:38 +0530

    ..

--- src/provider/index.jsx
+++ src/provider/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
+
 
 
 const queryClient = new QueryClient()
 
-
 function Providers({ children }) {
     return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </Provider>
     )
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
-
+import searchSlice from './searchSlice';
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        search: searchSlice,
+    }
 });
 
 export default store;
